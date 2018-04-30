50f73f0c94fef4d6b8a9ac43177bb81702998f57
Michael Lynch <mtlynch@users.noreply.github.com>
2018-04-30 11:29:01 -0400

    Adding empty home page (#4)
    * Adding empty home page
    
    * Fixing lint errors
    
    * Fixing root app test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+// Public pages
+import { HomePageComponent } from './public/home-page/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
