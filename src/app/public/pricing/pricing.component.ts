ac55d63da6f534c2b852e053335b1f3a93534418
Michael Lynch <git@mtlynch.io>
2018-08-09 17:27:52 -0400

    Fixing lint error

--- src/app/public/pricing/pricing.component.ts
+++ src/app/public/pricing/pricing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.scss']
+  styleUrls: ['./pricing.component.scss'],
 })
 export class PricingComponent implements OnInit {
 
