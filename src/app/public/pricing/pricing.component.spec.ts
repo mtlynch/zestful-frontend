bb8149c166cc418f79f9a54a4d810148298173bd
Michael Lynch <git@mtlynch.io>
2018-08-09 17:38:41 -0400

    Fixing broken test

--- src/app/public/pricing/pricing.component.spec.ts
+++ src/app/public/pricing/pricing.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { PricingComponent } from './pricing.component';
 
@@ -9,6 +10,7 @@ describe('PricingComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PricingComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
       .compileComponents();
   }));
